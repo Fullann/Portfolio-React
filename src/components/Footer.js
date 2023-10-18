60eda2862abca67a1d49b8048c4b1eb78e23e1e8
Nathan Füllemann <nathan.fullemann@gmail.com>
2023-10-18 10:20:35 +0200

    [UPDATE] Add logo made by fullann

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Social from './Social.js';
+import madeByLogo from "../Assets/image/fullannlogo/Group 3.svg";
 
 function Footer() {
   let date = new Date();
@@ -8,8 +9,8 @@ function Footer() {
   return (
     <Container fluid className="footer">
       <Row>
-        <Col md="4" className="footer-copywright">
-          <h3>Nathan Füllemann</h3>
+        <Col  md="4"  className="footer-copywright">
+          <img href="https://fullann.ch/" style={{width:"200px"}} src={madeByLogo} alt="brand" />
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year}</h3>
